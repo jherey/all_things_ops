55a486bdac8e46e46f36b9faa626d9fb2fcad0f8
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2020-12-04 16:05:11 +0100

    remove keys

--- terraform/Project1/main.tf
+++ terraform/Project1/main.tf
@@ -1,32 +1,45 @@
 provider "aws" {
   region  = "us-east-1"
-  access_key = "AKIAI2HT5XMEWOL72OGQ"
-  secret_key = "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"
+  # access_key = "xxxx"
+  # secret_key = "xxxx"
 }
 
-resource "aws_vpc" "first_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "production"
-  }
-}
+# 1. Create vpc
+# 2. Create Internet Gateway
+# 3. Create Custom Route Table
+# 4. Create a Subnet
+# 5. Associate subnet with Route Table
+# 6. Create Security Group to allow port 22, 80, 443
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+# 8. Assign an elastic IP to the network interface created in step 7
+# 9. Create Ubuntu server and install/enable apache2
 
-resource "aws_subnet" "subnet_1" {
-  vpc_id     = aws_vpc.first_vpc.id
-  cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "prod subnet"
-  }
-}
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-00ddb0e5626798373"
-  instance_type = "t2.micro"
-  tags = {
-    # "Name" = "ubuntu"
-  }
-}
+
+# resource "aws_vpc" "first_vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "production"
+#   }
+# }
+
+# resource "aws_subnet" "subnet_1" {
+#   vpc_id     = aws_vpc.first_vpc.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "prod subnet"
+#   }
+# }
+
+# resource "aws_instance" "my-first-server" {
+#   ami           = "ami-00ddb0e5626798373"
+#   instance_type = "t2.micro"
+#   tags = {
+#     # "Name" = "ubuntu"
+#   }
+# }
 
 # resource "<provider>_<resource_type>" "name" {
 
